ea9264f7e75d4b0c6e8d8aa823e57ed2173dd301
Yudann <yudannnnn80@gmail.com>
2025-01-11 10:28:31 +0700

    feat: add sign-in dummy function

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,24 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const signInClick = () => {
+    if (!username || !password) {
+      alert("Username dan Password tidak boleh kosong!");
+      return;
+    }
+
+    alert("Berhasil login");
+    router.push("/home");
+  };
+
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center">
       <div className="w-[80%] h-fit flex flex-col justify-center items-center p-8 bg-white rounded-xl shadow-md">
@@ -8,19 +26,25 @@ export default function LoginPage() {
         <h2 className="py-4 text-xl font-bold">Login</h2>
         <div className="flex flex-col w-full gap-5">
           <input
+            type="text"
             placeholder="Username"
-            className="w-full bg-gray-200 rounded-2xl p-4"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="w-full bg-gray-200 rounded-2xl p-4 mb-4"
           />
           <input
+            type="password"
             placeholder="Password"
-            className="w-full bg-gray-200 rounded-2xl p-4"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full bg-gray-200 rounded-2xl p-4 mb-4"
           />
-          <Link
-            href="/home"
+          <button
+            onClick={signInClick}
             className="py-3 bg-blue-400 text-center rounded-2xl mt-5 text-white font-bold text-base"
           >
             Login
-          </Link>
+          </button>
         </div>
         <div className="flex justify-center items-center mt-4">
           <p>Belum punya akun? </p>

22d34bf7b07878d871acdca60f9b215b44025ef6
Yudann <yudannnnn80@gmail.com>
2025-01-11 10:30:10 +0700

    feat: add navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,7 +2,7 @@ import HamburgerMenu from "../HamburgerMenu";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 right-0 left-0 px-4 py-5 flex items-center justify-between">
+    <nav className="fixed  backdrop-blur-sm top-0 right-0 left-0 px-4 py-5 flex items-center justify-between">
       <HamburgerMenu />
       <div className="h-5 w-5 bg-black rounded-full"></div>
     </nav>

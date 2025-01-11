4ebb94e7b27fab7108f316245172104ce8041536
Yudann <yudannnnn80@gmail.com>
2025-01-11 12:20:18 +0700

    chore: split homepage into several components

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import Activities from "./components/Activities";
+import RecommendationActivity from "./components/Recommendation";
+import UserProfile from "./components/UserProfile";
+
 export default function Homepage() {
+  const searchParams = useSearchParams();
+  const username = searchParams.get("username") || "User";
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center"></div>
+    <div className="w-auto h-fit flex flex-col items-center mb-20 ">
+      <UserProfile username={username} />
+      <RecommendationActivity />
+      <Activities />
+    </div>
   );
 }
--- src/components/Bakground.tsx
+++
@@ -1,7 +0,0 @@
-const Background = () => {
-  return (
-    <div className="absolute inset-0 bg-gradient-to-b from-purple-200 via-purple-400 to-purple-200 -z-10"></div>
-  );
-};
-
-export default Background;

3027483b74368bab3eaea2e86c8c3bbd40413b2d
Yudann <yudannnnn80@gmail.com>
2025-01-12 22:05:12 +0700

    style: update navbar style

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,14 @@
+import { FaHeart } from "react-icons/fa";
 import HamburgerMenu from "../HamburgerMenu";
 
 export default function Navbar() {
   return (
     <nav className="fixed z-[99] backdrop-blur-sm top-0 right-0 left-0 px-4 py-5 flex items-center justify-between">
       <HamburgerMenu />
-      <div className="h-5 w-5 bg-black rounded-full"></div>
+      <div className="flex items-center gap-2">
+        <FaHeart className="fill-red-600" />
+        30 Hari
+      </div>
     </nav>
   );
 }

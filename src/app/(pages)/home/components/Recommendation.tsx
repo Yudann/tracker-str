dcef274eb164ad46164ddb8f2c65c30490cf67a1
Yudann <yudannnnn80@gmail.com>
2025-01-13 17:37:08 +0700

    chore: add link

--- src/app/(pages)/home/components/Recommendation.tsx
+++ src/app/(pages)/home/components/Recommendation.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function RecommendationActivity() {
   return (
     <div className="w-full mt-10 flex flex-col">
@@ -7,40 +9,48 @@ export default function RecommendationActivity() {
       {/* Slider */}
       <div className="flex gap-6 overflow-x-auto py-5 no-scrollbar px-5">
         {/* Card 1 */}
-        <div
-          className="min-w-[300px] h-48 bg-cover bg-center backdrop-blur-md rounded-lg shadow-lg flex items-end p-4 text-white hover:scale-[1.02] transition-all duration-300"
-          style={{
-            backgroundImage: "url('/assets/membaca.png')",
-          }}
-        >
-          <h2 className="text-lg font-semibold">Membaca</h2>
-        </div>
+        <Link href="/kognitif/membaca">
+          <div
+            className="min-w-[300px] h-48 bg-cover bg-center backdrop-blur-md rounded-lg shadow-lg flex items-end p-4 text-white hover:scale-[1.02] transition-all duration-300"
+            style={{
+              backgroundImage: "url('/assets/membaca.png')",
+            }}
+          >
+            <h2 className="text-lg font-semibold">Membaca</h2>
+          </div>
+        </Link>
         {/* Card 2 */}
-        <div
-          className="min-w-[300px] h-48 bg-cover bg-center backdrop-blur-md rounded-lg shadow-lg flex items-end p-4 text-white hover:scale-[1.01] transition-all duration-300"
-          style={{
-            backgroundImage: "url('/assets/mendengar.png')",
-          }}
-        >
-          <h2 className="text-lg font-semibold">Mendengar</h2>
-        </div>
+        <Link href="/kognitif/berhitung">
+          <div
+            className="min-w-[300px] h-48 bg-cover bg-center backdrop-blur-md rounded-lg shadow-lg flex items-end p-4 text-white hover:scale-[1.01] transition-all duration-300"
+            style={{
+              backgroundImage: "url('/assets/berhitung.png')",
+            }}
+          >
+            <h2 className="text-lg font-semibold">Berhitung</h2>
+          </div>
+        </Link>
         {/* Card 3 */}
-        <div
-          className="min-w-[300px] h-48 bg-cover bg-center backdrop-blur-md rounded-lg shadow-lg flex items-end p-4 text-white hover:scale-[1.01] transition-all duration-300"
-          style={{
-            backgroundImage: "url('/assets/penalaran.png')",
-          }}
-        >
-          <h2 className="text-lg font-semibold">Penalaran</h2>
-        </div>
-        <div
-          className="min-w-[300px] h-48 bg-cover bg-center backdrop-blur-md rounded-lg shadow-lg flex items-end p-4 text-white hover:scale-[1.01] transition-all duration-300"
-          style={{
-            backgroundImage: "url('/assets/menulis.png')",
-          }}
-        >
-          <h2 className="text-lg font-semibold">Menulis</h2>
-        </div>
+        <Link href="/kognitif/penalaran">
+          <div
+            className="min-w-[300px] h-48 bg-cover bg-center backdrop-blur-md rounded-lg shadow-lg flex items-end p-4 text-white hover:scale-[1.01] transition-all duration-300"
+            style={{
+              backgroundImage: "url('/assets/penalaran.png')",
+            }}
+          >
+            <h2 className="text-lg font-semibold">Penalaran</h2>
+          </div>
+        </Link>
+        <Link href="motorik/gerakan-badan">
+          <div
+            className="min-w-[300px] h-48 bg-cover bg-center backdrop-blur-md rounded-lg shadow-lg flex items-end p-4 text-white hover:scale-[1.01] transition-all duration-300"
+            style={{
+              backgroundImage: "url('/assets/badan.png')",
+            }}
+          >
+            <h2 className="text-lg font-semibold">Peregangan Badan</h2>
+          </div>
+        </Link>
       </div>
     </div>
   );
